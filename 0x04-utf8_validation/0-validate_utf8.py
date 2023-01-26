8689c8debc81c480499c433f9c026df039f49d31
sipambi <96469754+sipambi@users.noreply.github.com>
2023-01-26 22:21:45 +0200

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,52 +1,28 @@
 #!/usr/bin/python3
 """
-Define validUTF8(data) function that validates whether a
-string of ints represents a valid UTF-8 encoding.
+UTF-8 Validation
 """
-from itertools import takewhile
 
 
-def int_to_bits(nums):
+def validUTF8(data) -> bool:
     """
-    Helper function
-    Convert ints to bits
+    Returns True if data is a valid UTF-8 encoding, else return False
+    :param data:
+    :return:
     """
-    for num in nums:
-        bits = []
-        mask = 1 << 8  # cause we have 8 bits per byte. adds up to (11111111)
-        while mask:
-            mask >>= 1
-            bits.append(bool(num & mask))
-        yield bits
-
-
-def validUTF8(data):
-    """
-    Takes a list of ints and returns true if the list is
-    a valid UTF-8 encoding, else returns false
-    Args:
-        data : List of ints representing possible UTF-8 encoding
-    Return:
-        bool : True or False
-    """
-    bits = int_to_bits(data)
-    for byte in bits:
-        # if single byte char, then valid. continue
-        if byte[0] == 0:
-            continue
-
-        # if here, byte is multi-byte char
-        ones = sum(takewhile(bool, byte))
-        if ones <= 1:
-            return False
-        if ones >= 4:  # UTF-8 can be 1 to 4 bytes long
-            return False
-
-        for _ in range(ones - 1):
-            try:
-                byte = next(bits)
-            except StopIteration:
+    num_bytes = 0
+    for byte in data:
+        mask = 1 << 7
+        if not num_bytes:
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
                 return False
-            if byte[0:2] != [1, 0]:
+        else:
+            if byte >> 6 != 0b10:
                 return False
-    return True
+        num_bytes -= 1
+    return num_bytes == 0
